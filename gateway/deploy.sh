1ec0906ff9c0fcf7808c535783d7ec548659316b
Patrick Moreau <patrick@drifterapps.com>
2026-03-25 00:13:06 -0400

    chore: cleanup docker compose files

--- gateway/deploy.sh
+++ gateway/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 source .env
-rsync -avz --exclude-from='.rsyncignore' --delete ./ ${HOST_USER}@${HOST}:~/${HOST_NAME} && ssh ${HOST_USER}@${HOST} "cd ~/${HOST_NAME} && docker compose up -d"
+rsync -avz --exclude-from='.rsyncignore' --delete ../shared/portainer-agent/ ${HOST_USER}@${HOST}:~/${HOST_NAME}/portainer-agent
+rsync -avz --exclude-from='.rsyncignore' --exclude='portainer-agent' --delete ./ ${HOST_USER}@${HOST}:~/${HOST_NAME} && ssh ${HOST_USER}@${HOST} "cd ~/${HOST_NAME} && docker compose up -d"
--- home/deploy.sh
+++ home/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 source .env
-rsync -avz --exclude-from='.rsyncignore' --delete ./ ${HOST_USER}@${HOST}:~/${HOST_NAME} && ssh ${HOST_USER}@${HOST} "cd ~/${HOST_NAME} && docker compose up -d"
+rsync -avz --exclude-from='.rsyncignore' --delete ../shared/traefik-kop/ ${HOST_USER}@${HOST}:~/${HOST_NAME}/traefik-kop
+rsync -avz --exclude-from='.rsyncignore' --exclude='traefik-kop' --delete ./ ${HOST_USER}@${HOST}:~/${HOST_NAME} && ssh ${HOST_USER}@${HOST} "cd ~/${HOST_NAME} && docker compose up -d"
